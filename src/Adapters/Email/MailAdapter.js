7481b9078f39a2157ba7babf417a2307d56e8729
creativesoftwareios <creativesoftwareios@gmail.com>
2016-05-03 12:30:33 +0300

    Update MailAdapter.js

--- src/Adapters/Email/MailAdapter.js
+++ src/Adapters/Email/MailAdapter.js
@@ -17,17 +17,6 @@ export class MailAdapter {
    * to provide HTML templates etc...
    */
   // sendVerificationEmail({ link, appName, user }) {}
-  sendVerificationEmail({link, user, appName, }) {
-    let text = "Hi,\n\n" +
-	      "Let's see if this works or not for " + user.get("email") + " with " + appName + "\n\n" +
-	      "" +
-	      "Click right now here to confirm it!!!!!\n" + link;
-	      
-	let newText = "<html><body><h1>My First Heading</h1><p>My first paragraph.</p></body></html>";
-    let to = user.get("email");
-    let subject = 'Please verify your e-mail for ' + appName;
-    return { newText, to, subject };
-  }
   // sendPasswordResetEmail({ link, appName, user }) {}
 }
 
